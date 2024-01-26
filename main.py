5c3c4c12f29ec4ea12b09751f9d4fe3bec8caf67
Luke Wood <l.wood0097@gmail.com>
2024-01-26 23:59:56 +0000

    Added infinite scrolling

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ screen = pygame.display.set_mode((1920, 1080))
 clock = pygame.time.Clock()
 running = True
 dt = 0
+speed = 600
+font = pygame.font.SysFont("didot.ttc", 72)
 
-scroll = [screen.get_width()/2, screen.get_height()/2]
+scroll = [0, 0]
 
 bg = pygame.image.load("./images/land_shallow_topo_8192.tif").convert()
 
@@ -20,23 +22,38 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill('black')
+    screen.fill("black")
 
     # RENDER YOUR GAME HERE
-    screen.blit(bg, scroll)
+    screen.blit(bg, ((-scroll[0], -scroll[1])))
+    screen.blit(bg, ((-scroll[0] + 8192, -scroll[1])))
+    screen.blit(bg, ((-scroll[0] + 8192 * 2, -scroll[1])))
+
+    screen.blit(bg, ((-scroll[0], -scroll[1] + 4096)))
+    screen.blit(bg, ((-scroll[0] + 8192, -scroll[1] + 4096)))
+    screen.blit(bg, ((-scroll[0] + 8192 * 2, -scroll[1] + 4096)))
+
+    screen.blit(bg, ((-scroll[0], -scroll[1] + 4096 * 2)))
+    screen.blit(bg, ((-scroll[0] + 8192, -scroll[1] + 4096 * 2)))
+    screen.blit(bg, ((-scroll[0] + 8192 * 2, -scroll[1] + 4096 * 2)))
+
+    img = font.render(f"Scroll: [{scroll[0]}, {scroll[1]}]", True, (255, 0, 0))
+    screen.blit(img, (20, 20))
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        scroll[1] += 600 * dt
+        scroll[1] -= speed * dt
     if keys[pygame.K_s]:
-        scroll[1] -= 600 * dt
+        scroll[1] += speed * dt
     if keys[pygame.K_a]:
-        scroll[0] += 600 * dt
+        scroll[0] -= speed * dt
     if keys[pygame.K_d]:
-        scroll[0] -= 600 * dt
+        scroll[0] += speed * dt
 
     # flip() the display to put your work on screen
     pygame.display.flip()
+
+    scroll = [scroll[0] % 8192, scroll[1] % 4096]
 
     # limits FPS to 60
     # dt is delta time in seconds since last frame, used for framerate-
