d8379be1a315c8030de213570007ff398737c7df
Billy Rebecchi <billy.rebecchi@googlemail.com>
2024-01-27 17:13:57 +0000

    Add collision detection to stay on land

--- entities.py
+++ entities.py
@@ -2,11 +2,19 @@ import pygame
 
 class Player:
 
-    def __init__(self, image_asset, scale, speed):
+    def __init__(self, screen, image_asset, scale, speed):
+        self.screen = screen
         self.image = pygame.image.load(image_asset)
         self.image = pygame.transform.scale(self.image, (scale, scale))
         self.speed = speed
+        self.scale = scale
         self.vertical_offset = 0
 
-    def render(self, screen):
-        screen.blit(self.image, (int(screen.get_width() / 2), int(screen.get_height() / 2) + self.vertical_offset))
+    def render(self):
+        self.screen.blit(self.image, (self.get_x(), self.get_y()))
+
+    def get_x(self):
+        return int(self.screen.get_width() / 2)
+    
+    def get_y(self):
+        return int(self.screen.get_height() / 2) + self.vertical_offset
