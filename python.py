70ba478da4ac614442a6c9e2e3ccff2a406981b8
Akosahh <137807567+Akosahh@users.noreply.github.com>
2024-01-27 00:00:54 +0000

    Added player (#7)

--- python.py
+++ python.py
@@ -1,4 +1,5 @@
 import pygame
+from PIL import Image
 
 
 class Background(pygame.sprite.Sprite):
@@ -12,10 +13,14 @@ pygame.init()
 screen = pygame.display.set_mode((600, 500))
 
 bg = Background("./images/map.jpg")
+player = pygame.image.load("./images/laughing.png")
+player = pygame.transform.scale(player, (30, 30))
 
 clock = pygame.time.Clock()
 running = True
+dt = 0
 
+player_position = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
 while running:
 
@@ -23,9 +28,20 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    screen.blit(bg.image, (0,0))
+    screen.blit(bg.image, (0, 0))
+    # pygame.draw.circle(screen, player, player_position, 10)
+    screen.blit(player, player_position)
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_position.y -= 300 * dt
+    if keys[pygame.K_s]:
+        player_position.y += 300 * dt
+    if keys[pygame.K_a]:
+        player_position.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player_position.x += 300 * dt
 
-    pygame.display.flip()
-    clock.tick(40)
+    pygame.display.update()
+    dt = clock.tick(40) / 10000
 
 pygame.quit()
