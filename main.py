a6fee177243e3914345b65d0a5111a6c2d6de53f
Luke Wood <l.wood0097@gmail.com>
2024-01-27 01:11:22 +0000

    Moved assets to own folder, combined python.py and main.py

--- main.py
+++ main.py
@@ -3,24 +3,25 @@ import math
 
 # pygame setup
 pygame.init()
+
 screen = pygame.display.set_mode((1920, 1080))
+font = pygame.font.SysFont("didot.ttc", 72)
 clock = pygame.time.Clock()
+pygame.mixer.init()
+
+bg = pygame.image.load("./assets/images/land_shallow_topo_8192.tif").convert()
+player = pygame.image.load("./assets/images/laughing.png")
+player = pygame.transform.scale(player, (30, 30))
+
+sound = pygame.mixer.Sound("./assets/sounds/main_laugh_1.wav")
+
 running = True
 dt = 0
 speed = 600
-font = pygame.font.SysFont("didot.ttc", 72)
-
 scroll = [0, 0]
 
-bg = pygame.image.load("./images/land_shallow_topo_8192.tif").convert()
-
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
+    # -------- DRAW --------
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("black")
 
@@ -28,8 +29,20 @@ while running:
         for j in range(3):
             screen.blit(bg, ((-scroll[0] + 8192 * i, -scroll[1] + 4096 * j)))
 
+    screen.blit(player, (screen.get_width() / 2, screen.get_height() / 2))
+    
     img = font.render(f"Scroll: [{scroll[0]}, {scroll[1]}]", True, (255, 0, 0))
     screen.blit(img, (20, 20))
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    # -------- Logic --------
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
@@ -40,9 +53,8 @@ while running:
         scroll[0] -= speed * dt
     if keys[pygame.K_d]:
         scroll[0] += speed * dt
-
-    # flip() the display to put your work on screen
-    pygame.display.flip()
+    if keys[pygame.K_ESCAPE]:
+        pygame.mixer.Sound.play(sound)
 
     scroll = [scroll[0] % 8192, scroll[1] % 4096]
 
