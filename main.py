7776feec416fd89e47229c2edbdba9b63c0388cb
Luke Wood <l.wood0097@gmail.com>
2024-01-27 00:53:47 +0000

    6 change background to map (#11)
    * Changed scrolling to work with map and updated map to Blue Marbel
    
    * Added infinite scrolling
    
    * Refactored with nested loop

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ import math
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((1920, 1080))
 clock = pygame.time.Clock()
 running = True
 dt = 0
+speed = 600
+font = pygame.font.SysFont("didot.ttc", 72)
 
-scroll = [screen.get_width()/2, screen.get_height()/2]
+scroll = [0, 0]
 
-bg = pygame.image.load("mountains-190055_1920.jpg").convert()
+bg = pygame.image.load("./images/land_shallow_topo_8192.tif").convert()
 
 while running:
     # poll for events
@@ -20,23 +22,29 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill('black')
+    screen.fill("black")
 
-    # RENDER YOUR GAME HERE
-    screen.blit(bg, scroll)
+    for i in range(3):
+        for j in range(3):
+            screen.blit(bg, ((-scroll[0] + 8192 * i, -scroll[1] + 4096 * j)))
+
+    img = font.render(f"Scroll: [{scroll[0]}, {scroll[1]}]", True, (255, 0, 0))
+    screen.blit(img, (20, 20))
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        scroll[1] -= 300 * dt
+        scroll[1] -= speed * dt
     if keys[pygame.K_s]:
-        scroll[1] += 300 * dt
+        scroll[1] += speed * dt
     if keys[pygame.K_a]:
-        scroll[0] -= 300 * dt
+        scroll[0] -= speed * dt
     if keys[pygame.K_d]:
-        scroll[0] += 300 * dt
+        scroll[0] += speed * dt
 
     # flip() the display to put your work on screen
     pygame.display.flip()
+
+    scroll = [scroll[0] % 8192, scroll[1] % 4096]
 
     # limits FPS to 60
     # dt is delta time in seconds since last frame, used for framerate-
